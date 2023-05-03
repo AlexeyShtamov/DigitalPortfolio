73e49db2e2c1580fcf3ddf3ee8beb559b5a21d19
AlexeyShtamov <alexshamox@gmail.com>
2023-05-03 16:18:08 +0500

    some css changes

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,25 +1,50 @@
 function SignOut(){
     $.ajax({
         url: 'singOut.php', //перенаправить пользователя на эту страницу
-            method: 'POST',
+        method: 'POST',
 
-            success: function(response) {
-                //обработка успешного ответа
-                window.location.href = 'login.php' //перенаправление на страницу входа
-            },
-            error: function(xhr, status, error) {
-                //обработка ответ об ошибке
-                console.log(error);
-            }
+        success: function(response) {
+            //обработка успешного ответа
+            window.location.href = 'login.php' //перенаправление на страницу входа
+        },
+        error: function(xhr, status, error) {
+            //обработка ответ об ошибке
+            console.log(error);
+        }
     });
 }
 
 function show_hide_password(target){
-	var input = document.getElementById('password__input');
-	if (input.getAttribute('type') == 'password') {
-		input.setAttribute('type', 'text');
-	} else {
-		input.setAttribute('type', 'password');
-	}
-	return false;
+    var input = document.getElementById('password__input');
+    if (input.getAttribute('type') == 'password') {
+        input.setAttribute('type', 'text');
+    } else {
+        input.setAttribute('type', 'password');
+    }
+    return false;
+}
+
+function sendProjectData(){
+    const projectData = {
+        name: document.getElementById('portfolio__input__name'),
+        file: document.getElementById('document.getElementById'),
+        description: document.getElementById('portfolio__link'),
+        avatar: document.getElementById('portfolio__logo')
+    }
+
+    const jsonData = JSON.stringify(projectData);
+
+    $.ajax({
+        url: "", //сюда вписать путь, куда эти данные отправить
+        type: "POST",
+        data: jsonData,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(response) {
+            console.log("отправлено на бэк");
+        },
+        error: function(error) {
+            console.error("ошибка при отправке на бэк", error);
+        }
+    });
 }
